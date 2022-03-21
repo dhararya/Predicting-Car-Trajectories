f9bfbc30094bfcaadf86d47bbcd778b13f425832
dhararya <aryan.dhar014@gmail.com>
2022-03-20 23:44:24 -0400

    loss

--- prediction/modules/probabilistic_model.py
+++ prediction/modules/probabilistic_model.py
@@ -6,10 +6,16 @@ from prediction.model import PredictionModel, PredictionModelConfig
 
 class ProbabilisticDecoder(nn.Module):
     ## write the code for the new model here
-    pass
+    nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128, 32),
+            nn.ReLU(),
+            nn.Linear(32, 2),
+            nn.ReLU(),
+        )
 
 
 class ProbabilisticModel(PredictionModel):
     def __init__(self, config: PredictionModelConfig) -> None:
         super().__init__(config)
-        self._decoder = ProbabilisticModel()
+        self._decoder = ProbabilisticDecoder()
