bca369fbca7c5e5d7bdf09dafdda188b39a0800d
dhararya <aryan.dhar014@gmail.com>
2022-03-28 22:31:01 -0400

    iou bug

--- tracking/matching.py
+++ tracking/matching.py
@@ -27,7 +27,7 @@ def greedy_matching(cost_matrix: np.ndarray) -> Tuple[List, List]:
     for i in range(min(M, N)):
         row_ids.append(i)
         col_ids.append(np.argmin(cost_matrix[i]))
-        cost[col_ids[i]] = 999
+        cost[:, col_ids[i]] = 999
     if N < M:
         row_ids, col_ids = col_ids, row_ids
     return row_ids, col_ids
